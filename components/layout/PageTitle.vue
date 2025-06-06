ffec4052c8163d3da1daebdfff4a5948a16ee31d
Marek Mikula <marek@justitpro.com>
2025-06-06 14:00:30 +0200

    feat. fix

--- components/layout/PageTitle.vue
+++ components/layout/PageTitle.vue
@@ -3,41 +3,34 @@
 
     <div class="md:flex md:items-center md:justify-between">
 
-      <!-- complete custom page title defined by slot -->
-      <slot v-if="$slots.default"/>
-
-      <template v-else>
-
-        <!-- page title with possible icon -->
-        <div class="min-w-0 flex-1">
-          <h2 class="text-2xl font-medium text-gray-900 flex items-center space-x-2">
-            <component v-if="icon" :is="icon" class="size-6 shrink-0"/>
-            <span v-if="title" class="truncate">
+      <!-- page title with possible icon -->
+      <div class="min-w-0 flex-1">
+        <h2 class="text-2xl font-medium text-gray-900 flex items-center space-x-2">
+          <component v-if="icon" :is="icon" class="size-6 shrink-0"/>
+          <span v-if="title" class="truncate">
             {{ title }}
           </span>
-            <slot name="afterTitle"/>
-          </h2>
-          <p v-if="subtitle" class="mt-1 text-sm text-gray-500">
-            {{ subtitle }}
-          </p>
-        </div>
+          <slot name="afterTitle"/>
+        </h2>
+        <p v-if="subtitle" class="mt-1 text-sm text-gray-500">
+          {{ subtitle }}
+        </p>
+      </div>
 
-        <!-- page title actions -->
-        <div v-if="actions && actions.length > 0" class="mt-3 flex md:mt-0 md:ml-3 space-x-2">
-          <CommonButton
-              v-for="(action, index) in actions"
-              :key="index"
-              :variant="action.variant"
-              :symmetrical="!!action.icon"
-              @click="action.handler"
-              v-tooltip="action.tooltip ? action.tooltip : false"
-          >
-            <span v-if="action.label">{{ action.label }}</span>
-            <component v-else-if="action.icon" :is="action.icon" class="size-5"/>
-          </CommonButton>
-        </div>
-
-      </template>
+      <!-- page title actions -->
+      <div v-if="actions && actions.length > 0" class="mt-3 flex md:mt-0 md:ml-3 space-x-2">
+        <CommonButton
+            v-for="(action, index) in actions"
+            :key="index"
+            :variant="action.variant"
+            :symmetrical="!!action.icon"
+            @click="action.handler"
+            v-tooltip="action.tooltip ? action.tooltip : false"
+        >
+          <span v-if="action.label">{{ action.label }}</span>
+          <component v-else-if="action.icon" :is="action.icon" class="size-5"/>
+        </CommonButton>
+      </div>
 
     </div>
 
