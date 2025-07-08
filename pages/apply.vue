90d2ab2dc7860e2022bf6d62c807089e27c77007
Marek Mikula <marek@justitpro.com>
2025-07-08 11:57:03 +0200

    feat: renamed suggest controller

---
+++ pages/apply.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    Apply for position! :) {{ token }}
+  </div>
+</template>
+
+<script setup lang="ts">
+const token = useRouteQuery<string>('token')
+
+definePageMeta({
+  layout: 'default',
+  async validate(route) {
+    return typeof route.query.token === 'string'
+  }
+})
+</script>
