640f007d6a819b2562ade28ad5270303810fbd54
Marek Mikula <marek@justitpro.com>
2025-07-22 16:29:37 +0200

    feat: updated scale map

--- components/common/Scale.vue
+++ components/common/Scale.vue
@@ -26,10 +26,6 @@ const percentage = computed<number>(() => {
 })
 
 const colorClass = computed<string>(() => {
-  if (percentage.value === 0.0) {
-    return ''
-  }
-
   for (const key in scaleMapper.value) {
     const passes = scaleMapper.value[key](percentage.value)
 
@@ -45,16 +41,11 @@ const colorClass = computed<string>(() => {
 
 function createDefaultScaleMapper(): ScaleMapper {
   return {
-    'bg-primary-50': (value) => value > 0 && value < 10,
-    'bg-linear-to-r from-primary-50 to-primary-50': (value) => value >= 10 && value < 20,
-    'bg-linear-to-r from-primary-50 to-primary-100': (value) => value >= 20 && value < 30,
-    'bg-linear-to-r from-primary-50 to-primary-200': (value) => value >= 30 && value < 40,
-    'bg-linear-to-r from-primary-50 to-primary-300': (value) => value >= 40 && value < 50,
-    'bg-linear-to-r from-primary-50 to-primary-400': (value) => value >= 50 && value < 60,
-    'bg-linear-to-r from-primary-50 to-primary-500': (value) => value >= 60 && value < 70,
-    'bg-linear-to-r from-primary-50 to-primary-600': (value) => value >= 70 && value < 80,
-    'bg-linear-to-r from-primary-50 to-primary-700': (value) => value >= 80 && value < 90,
-    'bg-linear-to-r from-primary-50 to-primary-800': (value) => value >= 90 && value <= 100,
+    'bg-linear-to-r from-primary-50 to-primary-100': (value) => value >= 0 && value < 20,
+    'bg-linear-to-r from-primary-50 to-primary-200': (value) => value >= 20 && value < 40,
+    'bg-linear-to-r from-primary-50 to-primary-300': (value) => value >= 40 && value < 60,
+    'bg-linear-to-r from-primary-50 to-primary-400': (value) => value >= 60 && value < 80,
+    'bg-linear-to-r from-primary-50 to-primary-500': (value) => value >= 80 && value <= 100,
   }
 }
 </script>
