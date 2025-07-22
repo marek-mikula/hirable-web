bfe766ef7a0608c6b54ad9ebb857f27a65382aca
Marek Mikula <marek@justitpro.com>
2025-07-22 14:24:57 +0200

    feat: new notification

--- types/enums.ts
+++ types/enums.ts
@@ -140,4 +140,6 @@ export enum NOTIFICATION_TYPE {
     POSITION_ASSIGNED_AS_HM = 'position:assigned_as_hm',
     POSITION_REMOVED_AS_RECRUITER = 'position:removed_as_recruiter',
     POSITION_REMOVED_AS_HM = 'position:removed_as_hm',
+    APPLICATION_ACCEPTED = 'application:accepted',
+    APPLICATION_NEW_CANDIDATE = 'application:new_candidate',
 }
