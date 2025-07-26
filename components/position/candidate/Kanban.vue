17700458ee8e385ae7063f6b786b07229ec63ad9
Marek Mikula <marek@justitpro.com>
2025-07-26 12:39:36 +0200

    feat: split kanban into multiple components

--- components/position/candidate/Kanban.vue
+++ components/position/candidate/Kanban.vue
@@ -19,47 +19,18 @@
     </div>
 
     <div class="overflow-x-auto flex flex-row flex-nowrap gap-3 lg:gap-4 scrollbar-hidden">
-      <div v-for="kanbanStep in visibleSteps" :key="kanbanStep.step.id" class="shrink-0 w-[350px] flex flex-col space-y-2">
-
-        <!-- kanban column header -->
-        <div class="flex items-center p-2 bg-gray-50 rounded-md border border-gray-200 space-x-2">
-          <FormCheckbox :name="`select-all-${kanbanStep.step.id}`" v-tooltip="{ content: $t('common.selectAll') }"/>
-          <h2 class="flex-1 min-w-0 text-lg font-medium truncate">
-            {{ kanbanStep.step.isCustom ? kanbanStep.step.step : $t(`model.processStep.steps.${kanbanStep.step.step}`) }}
-          </h2>
-          <div class="shrink-0 flex items-center space-x-2">
-            <CommonBadge variant="info" :label="$t('common.total', { n: kanbanStep.positionCandidates.length })"/>
-            <CommonButton variant="secondary" :size="2" symmetrical>
-              <EllipsisVerticalIcon class="size-4"/>
-            </CommonButton>
-          </div>
-        </div>
-
-        <!-- kanban column body -->
-        <div class="flex-col space-y-1">
-
-          <p v-if="kanbanStep.positionCandidates.length === 0" class="border border-dashed border-gray-200 p-3 lg:p-4 text-sm rounded-md text-gray-500">
-            Žádní kandidáti
-          </p>
-
-          <template v-else>
-            <PositionCandidateKanbanCard
-              v-for="positionCandidate in kanbanStep.positionCandidates"
-              :key="positionCandidate.id"
-              :position-candidate="positionCandidate"
-            />
-          </template>
-
-        </div>
-
-      </div>
+      <PositionCandidateKanbanColumn
+          v-for="kanbanStep in visibleSteps"
+          :key="kanbanStep.step.id"
+          :kanban-step="kanbanStep"
+      />
     </div>
 
   </div>
 </template>
 
 <script lang="ts" setup>
-import {MagnifyingGlassIcon, EllipsisVerticalIcon} from "@heroicons/vue/24/outline";
+import {MagnifyingGlassIcon} from "@heroicons/vue/24/outline";
 import type {KanbanStep} from "~/repositories/resources";
 import {searchInString} from "~/utils/helpers";
 
---
+++ components/position/candidate/KanbanColumn.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="shrink-0 w-[350px] flex flex-col space-y-2">
+
+    <!-- kanban column header -->
+    <div class="flex items-center p-2 bg-gray-50 rounded-md border border-gray-200 space-x-2">
+      <FormCheckbox :name="`select-all-${kanbanStep.step.id}`" v-tooltip="{ content: $t('common.selectAll') }"/>
+      <h2 class="flex-1 min-w-0 text-lg font-medium truncate">
+        {{ kanbanStep.step.isCustom ? kanbanStep.step.step : $t(`model.processStep.steps.${kanbanStep.step.step}`) }}
+      </h2>
+      <div class="shrink-0 flex items-center space-x-2">
+        <CommonBadge variant="info" :label="$t('common.total', { n: kanbanStep.positionCandidates.length })"/>
+        <CommonButton variant="secondary" :size="2" symmetrical>
+          <EllipsisVerticalIcon class="size-4"/>
+        </CommonButton>
+      </div>
+    </div>
+
+    <!-- kanban column body -->
+    <div class="flex-col space-y-1">
+      <p v-if="kanbanStep.positionCandidates.length === 0" class="border border-dashed border-gray-200 p-3 lg:p-4 text-sm rounded-md text-gray-500">
+        Žádní kandidáti
+      </p>
+      <template v-else>
+        <PositionCandidateKanbanCard
+            v-for="positionCandidate in kanbanStep.positionCandidates"
+            :key="positionCandidate.id"
+            :position-candidate="positionCandidate"
+        />
+      </template>
+    </div>
+
+  </div>
+</template>
+
+<script lang="ts" setup>
+import {EllipsisVerticalIcon} from "@heroicons/vue/24/outline";
+import type {KanbanStep} from "~/repositories/resources";
+
+const props = defineProps<{
+  kanbanStep: KanbanStep
+}>()
+</script>
