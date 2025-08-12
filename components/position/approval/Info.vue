d6de4aa87f29bbbf4620ca4cfc1ea8ad78869290
Marek Mikula <marek@justitpro.com>
2025-08-12 23:20:37 +0200

    feat: better text

--- components/position/approval/Info.vue
+++ components/position/approval/Info.vue
@@ -100,9 +100,7 @@
           <dt class="text-sm font-medium text-gray-900">
             {{ $t('model.position.description') }}
           </dt>
-          <dd class="mt-2 text-sm text-gray-700 sm:col-span-2">
-            {{ position.description }}
-          </dd>
+          <dd class="mt-2 text-sm text-gray-700 sm:col-span-2 whitespace-pre-line" v-text="position.description ?? '-'"/>
         </div>
 
         <div class="p-3">
@@ -210,9 +208,7 @@
           <dt class="text-sm font-medium text-gray-900">
             {{ $t('model.position.hardSkills') }}
           </dt>
-          <dd class="mt-2 text-sm text-gray-700 sm:col-span-2">
-            {{ position.hardSkills ?? '-' }}
-          </dd>
+          <dd class="mt-2 text-sm text-gray-700 sm:col-span-2 whitespace-pre-line" v-text="position.hardSkills ?? '-'"/>
         </div>
 
         <div class="p-3">
