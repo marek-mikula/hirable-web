39459e0eb69a8948d4696d7c0b9422fd6802491d
Marek Mikula <marek@justitpro.com>
2025-08-31 15:10:49 +0200

    feat: added new endpoint

--- repositories/positionCandidate/PositionCandidateRepositoryInterface.ts
+++ repositories/positionCandidate/PositionCandidateRepositoryInterface.ts
@@ -1,6 +1,7 @@
 import type {Response} from "~/types/request";
-import type {SetStepResponse} from "~/repositories/positionCandidate/responses";
+import type {SetStepResponse, ShowResponse} from "~/repositories/positionCandidate/responses";
 
 export interface PositionCandidateRepositoryInterface {
+    show(positionId: number, id: number): Promise<Response<'json', ShowResponse>>
     setStep(positionId: number, id: number, positionProcessStepId: number): Promise<Response<'json', SetStepResponse>>
 }
